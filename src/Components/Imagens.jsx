6def2f029dedf20861e0b2c389780e1dc31fe901
Ronaldo Vieira <ronaldovieira.lira@gmail.com>
2022-01-21 01:17:24 -0300

    redução de width da img e remoção dos titulos

--- src/Components/Imagens.jsx
+++ src/Components/Imagens.jsx
@@ -1,33 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Api from "../Data/Api";
 import typeResq from "../Data/TMDB";
-
+import "../CSS/Movie.css"
+import Skeleton from '@mui/material/Skeleton';
 
 export default function Movie(props) {
-    
-    const [api_Img, setApi_img] = useState()
+
+    const [request, setApi_img] = useState()
 
     useEffect(() => {
         Api
-            .get(typeResq("popular",props.num_Page))
+            .get(typeResq(props.type, props.genero))
             .then((resposta) => {
                 setApi_img(resposta.data)
             })
             .catch((erro) => {
-                console.log("Deu ruim brother")
+                console.log("Deu ruim brother" + erro)
             })
-    }, []); 
+    }, []);
+
 
     return (
         <>
             {
-                api_Img?.results.map((img, id) => {
+                request?.results.map((img, id) => {
                     return (
-                        <div className="caixaItem">
-                            <img src={`https://image.tmdb.org/t/p/w200${img?.poster_path}`}  key={id}/>
-                        </div>
+                            <div className="caixaItem">   
+                                <img src={`https://image.tmdb.org/t/p/w200${img?.poster_path}`} key={id} />
+                                {/* <h3>
+                                    { request?.results[id].title}
+                                </h3> */}
+                            </div>
                     )
-                })
+                })  
+
             }
         </>
     );
